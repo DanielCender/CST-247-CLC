e5704703e030eb997390810fce22dc8f33070b80
root <root@LAPTOP-VQSLNR9C.localdomain>
2021-04-27 21:45:45 -0500

    user data interface

---
+++ Minesweeper/Minesweeper/Data/UserDAOInterface.cs
@@ -0,0 +1,17 @@
+﻿using Minesweeper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Data
+{
+    interface UserDAOInterface
+    {
+        public User find(User model);
+        public User findBy(string field, string val);
+        public bool create(User model);
+        public bool update(User model);
+        public bool delete(User model);
+    }
+}
