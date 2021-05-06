b5da0031b76c516645c47f99fd8be14a0ab0e0bc
root <root@LAPTOP-VQSLNR9C.localdomain>
2021-05-06 12:45:04 -0500

    chore: some biz logic refactoring and plan doc bundle

--- Minesweeper/Minesweeper/Models/User.cs
+++ Minesweeper/Minesweeper/Models/User.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Minesweeper.Models
 {
     public class User
     {
         public int UserId { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
         public string FirstName { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
         public string LastName { get; set; }
+        [Required]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "The field Gender must be selected.")]
         public bool Gender { get; set; }
+        [Required]
+        [Range(0, 120, ErrorMessage = "Please enter valid integer Number")]
         public int Age { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
         public string State { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
         public string UserName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
         public string Password { get; set; }
 
         public User() { }
